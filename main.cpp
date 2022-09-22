ac27ea48176c3ce0feccc82a96c7ef540211af40
rachel <rachel.tranchida@gmail.com>
2022-09-22 09:24:49 +0200

    .gitignore + debut 1.5

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace  std;
+
+int main() {
+    double distance(0);
+    double prixTrain(0);
+    double consoVoiture(0);
+    double prixEssence(0);
+    double prixAmortissement(0);
+
+    cout << "Distance : ";
+    cin >> distance;
+    cout << "Prix du billet de train : ";
+    cin >> prixTrain;
+    cout << "Consommation de la voiture en L par 100 Km : ";
+    cin >> consoVoiture;
+    cout << "Prix de l'essence en CHF/L : ";
+    cin >> prixEssence;
+    cout << "Prix de l'amortissement de la voiture en CHF/Km : ";
+    cin >> prixAmortissement;
+
+    double prixTrajetVoiture = distance*consoVoiture/100*(prixEssence+prixEssence);
+    double prixTrajetTrain =prixTrain;
+    if(prixTrajetTrain < prixTrajetVoiture) {
+        cout << "Le trajet en train est moins cher" endl;
+    } else if(prixTrajetTrain) {
+
+    }
+    return 0;
+}
